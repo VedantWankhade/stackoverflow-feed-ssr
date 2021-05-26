838b78eaf7b5328af0fb60f277b8996191a68e02
VedantWankhade <vedantwankhade17@gmail.com>
2021-05-26 14:20:48 +0530

    Initial commit

---
+++ build/precache-manifest.25fe93cb5650adc69d31852ade6c02cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b16c9fcd45dcd042f8409874e0f61e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6880b2097e2f3d91f13e",
+    "url": "/static/js/2.074004ba.chunk.js"
+  },
+  {
+    "revision": "d0cb0a244b3563727e54b0f5d5982212",
+    "url": "/static/js/2.074004ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82990dc7ab9549e44d95",
+    "url": "/static/js/main.cb15ed23.chunk.js"
+  },
+  {
+    "revision": "29d80ccbbe305443cf59",
+    "url": "/static/js/runtime-main.40fb3df2.js"
+  }
+]);
